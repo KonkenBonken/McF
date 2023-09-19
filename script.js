a534d455174700c238a89b5234fae9cef3d1dac2
KonkenBonken <konrad05pettersson@gmail.com>
2023-09-19 23:59:31 +0200

    Extract online player list

--- script.js
+++ script.js
@@ -4,17 +4,20 @@ req.then(res => {
   if (!res.ok)
     return;
 
-  res.json().then(async (/** @type unknown */json) => {
-    if (!(
-      json != null && typeof json === 'object' &&
-      'online' in json && 'players' in json
-    )) return;
+  res.json()
+    .then(async (/** @type unknown */json) => {
+      if (!(
+        json != null && typeof json === 'object'
+        && 'online' in json && 'players' in json
+      )) return;
 
-    const { online, players } = json;
+      const { online, players } = json;
 
-    if (!(typeof online === 'boolean' && online))
-      return;
-    if (!(Array.isArray(players)))
-      return;
+      if (!(
+        typeof online === 'boolean' && online
+        && players != null && typeof players === 'object'
+      )) return;
+
+      const playerList = 'list' in players && Array.isArray(players.list) ? players.list : [];
   });
 });
